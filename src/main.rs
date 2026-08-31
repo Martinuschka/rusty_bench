5290b93aa34f251a4e875e0b2672587e5501e8fa
Martinuschka <95043342+Martinuschka@users.noreply.github.com>
2026-08-31 18:00:51 +0200

    reverse Commit 0999fe2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use core_affinity;
 use ctrlc;
-use rand::rngs::ThreadRng;
 use rand::Rng;
 use std::io::{self, Write};
 use std::sync::atomic::{AtomicBool, AtomicU64, Ordering};
@@ -169,7 +168,7 @@ fn main() {
                     // best-effort: ignore failures
                     core_affinity::set_for_current(core);
                 }
-                let mut rng: ThreadRng = rand::thread_rng();
+                let mut rng = rand::thread_rng();
                 while !stop.load(Ordering::Relaxed) {
                     // generate a batch of samples to amortize overhead
                     let batch = 1_000usize;
